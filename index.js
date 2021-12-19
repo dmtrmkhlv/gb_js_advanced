c992218449f3b11538343d9567bcd06f40511391
dmtrmkhlv <app@dmtrmkhlv.ru>
2021-12-19 17:52:43 +0300

    dz07

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const port = 3001
+const catalog_path = path.resolve(__dirname, './data/catalog.json');
+const cart_path = path.resolve(__dirname, './data/cart.json');
+const stats_path = path.resolve(__dirname, './data/stats.json');
+const static_dir = path.resolve(__dirname, './public/');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(static_dir));
+
+app.get('/catalogData', (req, res) => {
+    fs.readFile(catalog_path, 'utf8', (err, data) => {
+        res.send(data);
+    })
+});
+
+app.get('/cart', (req, res) => {
+    fs.readFile(cart_path, 'utf8', (err, data) => {
+        res.send(data);
+    })
+});
+
+app.post('/addToCart', (req, res) => {
+
+    fs.readFile(cart_path, 'utf8', (err, data) => {
+        let cart = JSON.parse(data);
+        let good = req.body;
+        let index = cart.findIndex((item) => item.id_product == good.id_product);
+
+        if (index != -1) {
+            cart[index].quantity = cart[index].quantity + 1;
+        } else {
+            cart.push(good);
+        }
+
+        fs.writeFile(cart_path, JSON.stringify(cart), (err) => {
+            console.log('done');
+            res.send(cart);
+            res.end();
+        });
+    });
+
+});
+
+app.post('/removeFromCart', (req, res) => {
+    fs.readFile(cart_path, 'utf8', (err, data) => {
+        let cart = JSON.parse(data);
+
+        const itemId = req.body.id_product;
+
+        let index = cart.findIndex((item) => item.id_product == itemId);
+
+        if (cart[index].quantity > 1) {
+            cart[index].quantity--;
+        } else {
+            cart.splice(index, 1);
+        }
+
+        fs.writeFile(cart_path, JSON.stringify(cart), (err) => {
+            console.log('done');
+            res.send(cart);
+            res.end();
+        });
+    });
+});
+
+app.post('/addToStats', (req, res) => {
+
+    fs.readFile(stats_path, 'utf8', (err, data) => {
+        let stats = JSON.parse(data);
+        let newStats = req.body;
+        stats.push(newStats);
+        fs.writeFile(stats_path, JSON.stringify(stats), (err) => {
+            console.log('stats add');
+            res.send(newStats);
+            res.end();
+        });
+    });
+
+});
+
+app.listen(port, function () {
+    console.log('server is running on port ' + port + '!')
+})
--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
-const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+const API_URL = 'http://127.0.0.1:3001';
 
-function makeGETRequest(URL, callback) {
-    fetch(URL)
+function makeGETRequest(URL, params = {}, callback) {
+    fetch(URL, params)
         .then((request) => {
             return request.json()
         })
@@ -102,7 +102,7 @@ new Vue({
     },
     methods: {
         fetchGoods() {
-            makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
+            makeGETRequest(`${API_URL}/catalogData`, {}, (goods) => {
                 if (goods) {
                     return this.goods = this.filteredGoods = goods;
                 }
@@ -111,9 +111,9 @@ new Vue({
             });
         },
         loadCart() {
-            makeGETRequest(`${API_URL}/getBasket.json`, (goods) => {
-                if (goods) {
-                    this.cartList = goods.contents;
+            makeGETRequest(`${API_URL}/cart`, {}, (goods) => {
+                if (goods && goods.length > 0) {
+                    this.cartList = goods;
                     this.sumGoods();
                 } else {
                     this.isVisibleError = !goods;
@@ -144,17 +144,25 @@ new Vue({
             this.isVisibleError = !this.isVisibleError;
         },
         addItem(good) {
-            makeGETRequest(`${API_URL}/addToBasket.json`, (data) => {
+            good.quantity = 1;
+            makeGETRequest(`${API_URL}/addToCart`, {
+                method: "POST",
+                headers: {
+                  'Content-Type': 'application/JSON'
+                },
+                body: JSON.stringify(good)
+              },
+              (data) => {
                 if (data) {
                     let index = this.cartList.findIndex((item) => item.id_product == good.id_product);
                     if (index != -1) {
                         this.cartList[index].quantity = this.cartList[index].quantity + 1;
-                        this.sumGoods();
-                        return;
+                    }else{
+                        good.quantity = 1;
+                        this.cartList.push(good);
                     }
-                    good.quantity = 1;
-                    this.cartList.push(good);
                     this.sumGoods();
+                    this.sendStats(good, "Добавление");
                 } else {
                     this.isVisibleError = !data;
                     this.isVisibleErroMessage = "Не удается добавить товар в корзину";
@@ -162,21 +170,41 @@ new Vue({
             });
         },
         removeItem(cartItem) {
-            makeGETRequest(`${API_URL}/deleteFromBasket.json`, (data) => {
+            makeGETRequest(`${API_URL}/removeFromCart`, {
+                method: "POST",
+                headers: {
+                  'Content-Type': 'application/JSON'
+                },
+                body: JSON.stringify(cartItem)
+              }, (data) => {
                 if (data) {
                     let index = this.cartList.findIndex((item) => item.id_product == cartItem.id_product);
                     if (this.cartList[index].quantity > 1) {
                         this.cartList[index].quantity--;
-                        this.sumGoods();
-                        return;
+                    }else{
+                        this.cartList.splice(index, 1);
                     }
-                    this.cartList.splice(index, 1);
                     this.sumGoods();
+                    this.sendStats(cartItem, "Удаление");
                 } else {
                     this.isVisibleError = !data;
                     this.isVisibleErroMessage = "Не удается добавить товар в корзину";
                 }
             });
+        },
+        sendStats(data, action){
+            let newStats = {
+                actionStats: action,
+                title: data.product_name,
+                time: new Date()
+            };
+            makeGETRequest(`${API_URL}/addToStats`, {
+                method: "POST",
+                headers: {
+                  'Content-Type': 'application/JSON'
+                },
+                body: JSON.stringify(newStats)
+              }, (data) => console.log(data));
         }
     },
     mounted() {
